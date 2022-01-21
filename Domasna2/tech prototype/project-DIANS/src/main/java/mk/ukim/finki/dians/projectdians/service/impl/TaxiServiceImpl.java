1fe73ee5eb4533897b699bab9eca8d44b383412a
DraganaTanevska <dragana.tanevska@yahoo.com>
2022-01-21 02:10:10 +0100

    commit1-domasna4

--- Domasna2/tech prototype/project-DIANS/src/main/java/mk/ukim/finki/dians/projectdians/service/impl/TaxiServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package mk.ukim.finki.dians.projectdians.service.impl;
-
-import mk.ukim.finki.dians.projectdians.service.TaxiService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TaxiServiceImpl implements TaxiService {
-}
--- Domasna2/tech prototype/project-DIANS/src/main/java/mk/ukim/finki/dians/projectdians/service/impl/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package mk.ukim.finki.dians.projectdians.service.impl;
-
-import mk.ukim.finki.dians.projectdians.service.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-}
