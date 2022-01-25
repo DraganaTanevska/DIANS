9239cdbf5cca25bb68aedef5a1a0d3545f71eb49
DraganaTanevska <dragana.tanevska@yahoo.com>
2022-01-25 02:21:47 +0100

    First microservice - v1
    microserviceOne created.

---
+++ microserviceOne/src/main/java/mk/ukim/finki/dians/microserviceOne/MicroserviceOne.java
@@ -0,0 +1,11 @@
+package mk.ukim.finki.dians.microserviceOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceOne {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceOne.class,args);
+    }
+}
