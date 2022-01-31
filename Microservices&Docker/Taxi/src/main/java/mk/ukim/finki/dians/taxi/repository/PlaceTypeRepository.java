bfba3c8de098925c0a29c0245120eef615c815de
DraganaTanevska <94012629+DraganaTanevska@users.noreply.github.com>
2022-01-31 22:20:41 +0100

    Add files via upload

---
+++ Microservices&Docker/Taxi/src/main/java/mk/ukim/finki/dians/taxi/repository/PlaceTypeRepository.java
@@ -0,0 +1,8 @@
+package mk.ukim.finki.dians.taxi.repository;
+
+import mk.ukim.finki.dians.taxi.model.PlaceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceTypeRepository extends JpaRepository<PlaceType, Long> {
+
+}
---
+++ Microservices&Docker/Taxi/src/main/java/mk/ukim/finki/dians/taxi/repository/TaxiRepository.java
@@ -0,0 +1,19 @@
+package mk.ukim.finki.dians.taxi.repository;
+
+import mk.ukim.finki.dians.taxi.model.Taxi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaxiRepository extends JpaRepository<Taxi, Long> {
+    List<Taxi> findAllByNameContains(String name);
+
+    List<Taxi> findAllByFinalRatingAfter(double Rating);
+
+    List<Taxi> findAllByOrderByFinalRatingAsc();
+
+    List<Taxi> findAllByOrderByNameAsc();
+    //  List<Taxi> findAllByPlaceType(PlaceType placeType);
+}
